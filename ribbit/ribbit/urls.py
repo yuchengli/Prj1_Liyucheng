0c87d835a446af9a7c8c32f2e496432c5f8a1dda
yuchengli <1261744019@qq.com>
2015-02-05 01:47:42 -0500

    twitter clone 2/5 check

--- ribbit/ribbit/urls.py
+++ ribbit/ribbit/urls.py
@@ -11,4 +11,8 @@ urlpatterns = patterns('',
     url(r'^login$', 'ribbit_app.views.login_view'), # login
     url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
     url(r'^signup$', 'ribbit_app.views.signup'), # signup
+    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit
+    url(r'^users/$', 'ribbit_app.views.users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
+    url(r'^follow$', 'ribbit_app.views.follow'),
 )
